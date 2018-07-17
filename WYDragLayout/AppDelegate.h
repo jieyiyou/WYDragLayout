055bf0030d2e091cdad21b5f2f9f2115985b7987
jieyiyou <13525560629@163.com>
2018-07-17 16:07:45 +0800

    Initial Commit

---
+++ WYDragLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WYDragLayout
+//
+//  Created by 薇谙 on 2018/7/17.
+//  Copyright © 2018年 WY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WYDragLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WYDragLayout
+//
+//  Created by 薇谙 on 2018/7/17.
+//  Copyright © 2018年 WY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
