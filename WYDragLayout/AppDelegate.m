f47b9c096e989894f9edbbc03e848b05f26b7ce7
jieyiyou <13525560629@163.com>
2018-07-19 17:49:15 +0800

    new

--- WYDragLayout/AppDelegate.m
+++ WYDragLayout/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,11 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
     // Override point for customization after application launch.
+    ViewController *fc = [[ViewController alloc] init];
+    UINavigationController *navCtrlr = [[UINavigationController alloc] initWithRootViewController:fc];
+    [self.window setRootViewController:navCtrlr];
     return YES;
 }
 
--- WYDragLayout/ViewController.m
+++ WYDragLayout/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "DetailViewController.h"
 
 @interface ViewController ()
+
+@property (nonatomic,strong)UIButton *detailButton;
 
 @end
 
@@ -16,14 +19,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.detailButton];
 }
 
+- (void)detailButtonClick
+{
+    DetailViewController *controller = [[DetailViewController alloc] init];
+    [self.navigationController pushViewController:controller animated:YES];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark --- lazy ---
+- (UIButton *)detailButton
+{
+    if (!_detailButton) {
+        _detailButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        _detailButton.frame = CGRectMake(0, (KScreenHeight - 20)/2.0, kScreenWidth, 20);
+        [_detailButton setTitle:@"商品详情页" forState:UIControlStateNormal];
+        _detailButton.titleLabel.font = kFontBold(20 );
+        [_detailButton setTitleColor:kRGB(50, 50, 50) forState:UIControlStateNormal];
+        [_detailButton addTarget:self action:@selector(detailButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _detailButton;
+}
 
 @end
